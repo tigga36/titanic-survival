4fbe54145b05a84f006ab2f0364fe396ec5de838
tigga36 <onotaiga36@gmail.com>
2019-04-11 23:39:57 +0900

    Split training data to train/test

--- data/prep.py
+++ data/prep.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 TITANIC_PATH = os.path.join("raw", "titanic")
 
@@ -8,4 +9,15 @@ def load_titanic_data(titanic_path=TITANIC_PATH):
     return pd.read_csv(csv_path)
 
 titanic = load_titanic_data()
-print(titanic.head())
+
+#random seed
+np.random.seed(21)
+
+def split_train_test(data, test_ratio):
+    shuffled_indicies = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indicies = shuffled_indicies[:test_set_size]
+    train_indicies = shuffled_indicies[test_set_size:]
+    return data.iloc[train_indicies], data.iloc[test_indicies]
+
+train_set, test_set = split_train_test(titanic, 0.2)
