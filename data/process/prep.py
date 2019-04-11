f8bf1e026420777e6f02fd3cb5becb4bc0224c25
tigga36 <onotaiga36@gmail.com>
2019-04-11 23:37:28 +0900

    Load raw test data

---
+++ data/process/prep.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+TITANIC_PATH = os.path.join("raw", "titanic")
+
+def load_titanic_data(titanic_path=TITANIC_PATH):
+    csv_path = os.path.join(titanic_path,"train.csv")
+    return pd.read_csv(csv_path)
+
+titanic = load_titanic_data()
+titanic.head()
