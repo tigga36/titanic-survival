3321722bfd4dc02a4b9a9ad865e633d158242e9d
tigga36 <onotaiga36@gmail.com>
2019-04-26 09:33:59 +0900

    Settle for single layer neural network as it has produced best results

--- titanic_project/networks/densenet.py
+++ titanic_project/networks/densenet.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow import keras
 from functools import partial
 
 from datetime import datetime
@@ -24,11 +23,11 @@ y = tf.placeholder(tf.int64, shape=(None), name="y")
 
 training = tf.placeholder_with_default(False, shape=(), name='training')
 
-dropout_rate = 0.8
+dropout_rate = 0.1
 
 scale = 0.001
 he_init = tf.contrib.layers.variance_scaling_initializer()
-my_layer_pre = partial(tf.layers.dense, kernel_initializer=he_init, kernel_regularizer=tf.contrib.layers.l1_regularizer(scale))
+my_layer_pre = partial(tf.layers.dense, kernel_initializer=he_init, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale))
 my_batch_norm = partial(tf.layers.batch_normalization, training=training, momentum=0.9)
 my_batch_drop = partial(tf.layers.dropout, dropout_rate, training=training)
 
@@ -38,27 +37,27 @@ with tf.name_scope("dnn1"):
     hidden1_drop = tf.layers.dropout(hidden1_batch, dropout_rate, training=training)
     hidden1_act = tf.nn.elu(hidden1_drop)
 
-    hidden2_pre = my_layer_pre(hidden1_act, n_hidden2, name="hidden2")
-    hidden2_batch = my_batch_norm(hidden2_pre)
-    hidden2_drop = tf.layers.dropout(hidden2_batch, dropout_rate, training=training)
-    hidden2_act = tf.nn.elu(hidden2_drop)
+    # hidden2_pre = my_layer_pre(hidden1_act, n_hidden2, name="hidden2")
+    # hidden2_batch = my_batch_norm(hidden2_pre)
+    # hidden2_drop = tf.layers.dropout(hidden2_batch, dropout_rate, training=training)
+    # hidden2_act = tf.nn.elu(hidden2_drop)
 
-    hidden3_pre = my_layer_pre(hidden2_act, n_hidden3, name="hidden3")
-    hidden3_batch = my_batch_norm(hidden3_pre)
-    hidden3_drop = tf.layers.dropout(hidden3_batch, dropout_rate, training=training)
-    hidden3_act = tf.nn.elu(hidden3_drop)
+    # hidden3_pre = my_layer_pre(hidden2_act, n_hidden3, name="hidden3")
+    # hidden3_batch = my_batch_norm(hidden3_pre)
+    # hidden3_drop = tf.layers.dropout(hidden3_batch, dropout_rate, training=training)
+    # hidden3_act = tf.nn.elu(hidden3_drop)
+    #
+    # hidden4_pre = my_layer_pre(hidden3_act, n_hidden4, name="hidden4")
+    # hidden4_batch = my_batch_norm(hidden4_pre)
+    # hidden4_drop = tf.layers.dropout(hidden4_batch, dropout_rate, training=training)
+    # hidden4_act = tf.nn.elu(hidden4_drop)
+    #
+    # hidden5_pre = my_layer_pre(hidden4_act, n_hidden5, name="hidden5")
+    # hidden5_batch = my_batch_norm(hidden5_pre)
+    # hidden5_drop = tf.layers.dropout(hidden5_batch, dropout_rate, training=training)
+    # hidden5_act = tf.nn.elu(hidden5_drop)
 
-    hidden4_pre = my_layer_pre(hidden3_act, n_hidden4, name="hidden4")
-    hidden4_batch = my_batch_norm(hidden4_pre)
-    hidden4_drop = tf.layers.dropout(hidden4_batch, dropout_rate, training=training)
-    hidden4_act = tf.nn.elu(hidden4_drop)
-
-    hidden5_pre = my_layer_pre(hidden4_act, n_hidden5, name="hidden5")
-    hidden5_batch = my_batch_norm(hidden5_pre)
-    hidden5_drop = tf.layers.dropout(hidden5_batch, dropout_rate, training=training)
-    hidden5_act = tf.nn.elu(hidden5_drop)
-
-    logits_pre = tf.layers.dense(hidden5_act, n_outputs, name="output")
+    logits_pre = tf.layers.dense(hidden1_act, n_outputs, name="output")
     logits = my_batch_norm(logits_pre)
 
 with tf.name_scope("loss"):
